de4ec38c57cf87c2d86c9e7de8c0c592301d7c7d
BSJ0311 <bangseojunnamja@gmail.com>
2026-06-02 19:45:32 +0900

    Add files via upload

---
+++ sources/wall/my.js
@@ -0,0 +1,10 @@
+const block = document.getElementById("block");
+
+for(let i = 0; i < 21; i++){
+
+    const img = document.createElement("img");
+
+    img.src = "images/img1.jpg";
+
+    block.appendChild(img);
+}
