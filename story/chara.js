a52c375bf0bbcdb27d272928049939b4475dd750
BSJ0311 <bangseojunnamja@gmail.com>
2026-06-05 23:03:30 +0900

    스토리.js

--- story/chara.js
+++ story/chara.js
@@ -1,15 +1,15 @@
 const characters = {
-    hero: { name: "주인공", portrait: "character1.jpg" },
-    villain: { name: "악당", portrait: "character2.jpg" },
-    girlfriend: { name: "여친", portrait: "character3.jpg" }
+    hero: { name: "Nyan Cat", portrait: "character1.jpg" },
+    villain: { name: "Dark Choco Cat", portrait: "character2.jpg" },
+    girlfriend: { name: "Cream Cat", portrait: "character3.jpg" }
 };
 
 const dialogueScript = [
     { character: characters.villain, text: "하하... 드디어 여기까지 왔군. 하지만 이미 늦었다.", portraitId: "villainPortrait" },
-    { character: characters.hero, text: "악당! 그녀는 어디 있지? 당장 그녀를 돌려줘!", portraitId: "heroPortrait" },
+    { character: characters.hero, text: "Dark Choco Cat! Cream Cat는 어디 있지? 당장 그녀를 돌려줘!", portraitId: "heroPortrait" },
     { character: characters.villain, text: "돌려달라고? 어림도 없지. 나는 그녀를 내 시종으로 부릴것이다.", portraitId: "villainPortrait" },
-    { character: characters.hero, text: "그만둬! 그녀를 이용하게 두지 않겠어!", portraitId: "heroPortrait" },
-    { character: characters.girlfriend, text: "주인공...! 오지마! 이건 위험한 함정이야..!", portraitId: "girlfriendPortrait" },
+    { character: characters.hero, text: "그만둬! Cream Cat를 이용하게 두지 않겠어!", portraitId: "heroPortrait" },
+    { character: characters.girlfriend, text: "Nyan Cat...! 오지마! 이건 위험한 함정이야..!", portraitId: "girlfriendPortrait" },
     { character: characters.hero, text: "여친! 기다려, 내가 지금 구하러 갈게!", portraitId: "heroPortrait" },
     { character: characters.villain, text: "크하하! 정말 눈물겹군. 그럼 어디 실력 발휘 좀 해보시지!", portraitId: "villainPortrait" },
     { character: characters.hero, text: "거기서!!!", portraitId: "heroPortrait" },
